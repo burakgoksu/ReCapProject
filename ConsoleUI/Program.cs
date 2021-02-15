6692e241af5e5c1e57363611e4f438b5661258fc
BURAK GÖKSU <gogsub58365@gmail.com>
2021-02-15 14:06:08 +0300

    8.ödev sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+
             CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var car in carManager.GetAll())
